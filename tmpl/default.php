a80204dd50534661c768c1b39df8fb2ab6230d64
Leonidas <leonidas78@mail.ru>
2015-08-17 15:54:34 +0300

    Create default.php

---
+++ tmpl/default.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+* @author Dallas Moore
+* Email : Dallas@viperwebistes.com
+* URL : www.viperwebsites.com
+* Description : This module displays icon links to your social media profiles.
+* Copyright (c) 2008-2010 Viper Web Solutions
+* License GNU GPL
+***/
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+	</div>
+
+<div class="clr"></div>
